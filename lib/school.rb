4ce61dd6f3ffabcadd69633426e2f91e88e6c21b
Will Figueiredo <will@wfigueiredo.com>
2018-10-25 02:59:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,25 @@
 # code here!
 class School
-  attr_accessor = :roster, :add_student, :grade
+  attr_accessor = :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, array|
+      array.sort!
+    end
+  end
 end
